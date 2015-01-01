8e2754d46792ac51f4451bb18a90fc843ce34105
Eric van Zanten <eric.vanzanten@gmail.com>
2015-01-01 15:44:18 -0600

    Trying to get Travis going

--- tests/__init__.py
+++ tests/__init__.py
@@ -24,12 +24,6 @@ def setUpPackage():
     ''' 
     Do database setup
     '''
-    conn = psycopg2.connect(conn_str)
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curs = conn.cursor()
-    curs.execute('CREATE DATABASE {0}'.format(dbname))
-    curs.close()
-    conn.close()
     init_db(eng=engine, sess=session)
     user = User('bob', 'bobspw', 'bob@bob.com')
     group = session.query(Group).first()
@@ -45,10 +39,4 @@ def setUpPackage():
 def tearDownPackage():
     session.close_all()
     engine.dispose()
-    conn = psycopg2.connect(conn_str)
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curs = conn.cursor()
-    curs.execute('DROP DATABASE {0}'.format(dbname))
-    curs.close()
-    conn.close()
     
