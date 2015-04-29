8f3be2e3e8bf27ba18aeb023be5ff6b7277f2b84
Eric van Zanten <eric.vanzanten@gmail.com>
2015-04-29 14:42:52 -0500

    Adding numpy to install steps

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 chown -R datamade.www-data /home/datamade
+/home/datamade/.virtualenvs/dedupe-api/bin/pip install numpy
 /home/datamade/.virtualenvs/dedupe-api/bin/pip install -r /home/datamade/dedupe-api/requirements.txt --upgrade
 aws s3 cp s3://datamade-codedeploy/configs/dedupeapi_app_config.py /home/datamade/dedupe-api/api/app_config.py --region us-east-1
 /home/datamade/.virtualenvs/dedupe-api/bin/python /home/datamade/dedupe-api/init_db.py
