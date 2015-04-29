4c05a0ce11858992fb915dac42f98544819dd50a
Eric van Zanten <eric.vanzanten@gmail.com>
2015-04-29 12:05:01 -0500

    Updating after_install to do the right thing (hopefully)

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 /home/datamade/.virtualenvs/pip install -r /home/datamade/dedupe-api/requirements.txt --upgrade
-aws s3 cp s3://datamade-codedeploy/configs/dedupeapi_app_config.py . --region us-east-1
-mv dedupeapi_app_config.py /home/datamade/dedupe-api/api/app_config.py
+aws s3 cp s3://datamade-codedeploy/configs/dedupeapi_app_config.py /home/datamade/dedupe-api/api/ --region us-east-1
 /home/datamade/.virtualenvs/python /home/datamade/dedupe-api/init_db.py
