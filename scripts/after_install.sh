e1fce8877c5d55958fec6266637efe6d1361e412
Eric van Zanten <eric.vanzanten@gmail.com>
2015-04-29 13:12:38 -0500

    Trying to actually get the deployment pushed through

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-/home/datamade/.virtualenvs/pip install -r /home/datamade/dedupe-api/requirements.txt --upgrade
-aws s3 cp s3://datamade-codedeploy/configs/dedupeapi_app_config.py /home/datamade/dedupe-api/api/ --region us-east-1
-/home/datamade/.virtualenvs/python /home/datamade/dedupe-api/init_db.py
+sudo chown -R datamade.www-data /home/datamade
+/home/datamade/.virtualenvs/bin/pip install -r /home/datamade/dedupe-api/requirements.txt --upgrade
+aws s3 cp s3://datamade-codedeploy/configs/dedupeapi_app_config.py /home/datamade/dedupe-api/api/app_config.py --region us-east-1
+/home/datamade/.virtualenvs/bin/python /home/datamade/dedupe-api/init_db.py
