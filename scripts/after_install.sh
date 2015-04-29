43f326819c5caf5aed4b1350b61f05d91254898c
Eric van Zanten <eric.vanzanten@gmail.com>
2015-04-29 13:31:21 -0500

    Aaagh. Might this actually deploy this time?

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo chown -R datamade.www-data /home/datamade
-/home/datamade/.virtualenvs/bin/pip install -r /home/datamade/dedupe-api/requirements.txt --upgrade
+chown -R datamade.www-data /home/datamade
+/home/datamade/.virtualenvs/dedupe-api/bin/pip install -r /home/datamade/dedupe-api/requirements.txt --upgrade
 aws s3 cp s3://datamade-codedeploy/configs/dedupeapi_app_config.py /home/datamade/dedupe-api/api/app_config.py --region us-east-1
-/home/datamade/.virtualenvs/bin/python /home/datamade/dedupe-api/init_db.py
+/home/datamade/.virtualenvs/dedupe-api/bin/python /home/datamade/dedupe-api/init_db.py
